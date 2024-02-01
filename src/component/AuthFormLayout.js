dfcd6444ffac4e02893528d758acd7f7b6c5635e
brijeshranpariya <brijeshranpariyak@gmail.com>
2024-02-01 21:41:49 +0530

    update-2

--- src/component/AuthFormLayout.js
+++ src/component/AuthFormLayout.js
@@ -1,8 +1,9 @@
 import logoDarkImg from "../assets/images/logoDarkImg.png"
 import logoLightImg from "../assets/images/logoLightImg.jpg"
-import show from '../assets/images/show.png';
-import hide from '../assets/images/hide.png';
+// import show from '../assets/images/show.png';
+// import hide from '../assets/images/hide.png';
 import React from 'react';
+import { LuEyeOff, LuEye } from 'react-icons/lu';
 import { useState } from 'react';
 function AuthFormLayout1() {
     const [isActive, setActive] = useState(false);
@@ -46,7 +47,7 @@ function AuthFormLayout1() {
 
 
     return (
-        <div className="flex h-lvh sm:justify-center justify-center lg:justify-start items-center border-2 border-red-800">
+        <div className="flex h-lvh sm:justify-center justify-center lg:justify-start items-center">
             <div className="mx-12 flex items-center justify-center lg:max-w-lg">
                 <div className="flex h-full flex-col">
                     <div className="shrink">
@@ -67,29 +68,31 @@ function AuthFormLayout1() {
                             </a>
                         </div>
                         <div className="py-6">
-                            <h1 className="mb-2 font-bold text-3xl text-default-800">
+                            <h1 className="mb-2 font-bold text-[35px] text-default-800">
                                 Login
                             </h1>
-                            <p className="max-w-md text-sm text-gray-600 text-default-500">Enter your credentials to access Flashpackers.</p>
+                            <p className="max-w-md w-64 sm:w-96 text-lg text-gray-600 text-default-500">Enter your credentials to access Flashpackers.</p>
                         </div>
                     </div>
                     <form className="flex flex-col relative ">
-                        <label className="my-3" htmlFor="email">Email</label>
-                        <input className="py-3 border-2 border-gray-200 pl-3 sm:w-96 w-64 rounded-lg placeholder-gray-500" onBlur={showEmail} name="email" type="text" placeholder="Enter your email" required />
+                        <label className="my-3 lg:text-xl" htmlFor="email">Email</label>
+                        <input className="py-3 px-4 border-2 lg:w-[400px] lg:h-14 border-gray-200 pl-3 sm:w-96 w-64 rounded-lg placeholder:text-lg placeholder-gray-600" onBlur={showEmail} name="email" type="text" placeholder="Enter your email" required />
                         <span id="enteremail" className="text-red-700 hidden">Please enter your Email</span>
                         <span id="invalidemail" className="text-red-700 hidden">Please enter valid Email</span>
-                        <label className="my-3" htmlFor="password">Password</label>
-                        <div className="relative">
-                            <input className=" py-3 border-2 border-gray-200 sm:w-96 w-64 rounded-lg" name="password" onBlur={showPass} type="password" required />
+                        <label className="my-3 relative  lg:text-xl top-4" htmlFor="password">Password</label>
+                        <div className="relative top-4">
+                            <input className=" py-3 pr-[74px] pl-4 border-2 lg:w-[400px] lg:h-14 border-gray-200 sm:w-96 w-64 rounded-lg" name="password" onBlur={showPass} type="password" required />
                             <span className="bg-gray-200 w-[1px] h-12 absolute top-[2px] right-[60px]"></span>
-                            {!isActive && <img onClick={toggleeye} className="absolute sm:right-[18px] right-[30px] bottom-[12px]" src={show} alt="" height={"25px"} width={"25px"} ></img>}
-                            {isActive && <img onClick={toggleeye} className="absolute sm:right-[18px] right-[30px] bottom-[12px]" src={hide} alt="" height={"25px"} width={"25px"} ></img>}
+
+                            {!isActive && <LuEyeOff onClick={toggleeye} size={20} className="text-default-600 absolute sm:right-[20px] right-[20px] bottom-[16px]" />}
+                            {isActive && <LuEye onClick={toggleeye} size={20} className="text-default-600 absolute sm:right-[20px] right-[20px] bottom-[16px]" />}
+
                         </div>
                         <span id="invalidpass" className="text-red-700 hidden">Invalid password</span>
                         <span id="enterpass" className="text-red-700 hidden">please enter the password</span>
                         <button
                             type="submit"
-                            className="mt-6 sm:w-96 w-64 shadow-lg shadow-gray-400 rounded-lg bg-green-500 px-6 py-3 text-base capitalize text-black transition-all hover:bg-green-400"
+                            className="relative top-12 sm:w-96 lg:w-[400px] lg:h-14 w-64 shadow-lg shadow-gray-400 rounded-lg bg-green-500 px-6 py-3 text-base text-black transition-all hover:bg-green-400"
                         >
                             Log In
                         </button>
